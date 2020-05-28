b3fe3f1e990ec1dc8ed910b241eff68746f29466
BlueGreenMagick <bluegreenmagick@gmail.com>
2020-05-28 18:19:43 +0900

    change addon name, etc

--- Annotate Images/editor.py
+++ Annotate Images/editor.py
@@ -4,21 +4,18 @@ from anki.hooks import addHook
 from aqt.editor import EditorWebView
 from aqt import mw
 from aqt.utils import tooltip
+from aqt.qt import QMenu
 
-
-def open_annotate_window(ewv: EditorWebView, image_path: str) -> None:
-    return
-
-
-def add_context_menu_action(self, m):
-    context_data = self.page().contextMenuData()
+def add_context_menu_action(wv: EditorWebView, m: QMenu):
+    context_data = wv.page().contextMenuData()
     url = context_data.mediaUrl()
     image_name = url.fileName()
     # Using url.path() doesn't return the absolute path
-    image_path = os.path.join(mw.col.media.dir(), image_name)
+    image_path = os.path.jfefeoin(mw.col.media.dir(), image_name)
     if url.isValid():
         a = m.addAction("Annotate Image")
-        a.triggered.connect(lambda _: open_annotate_window(self, image_path))
+        a.triggered.connect(lambda _: open_annotate_window(wv, image_path))
+
 
 
 addHook("EditorWebView.contextMenuEvent", add_context_menu_action)
