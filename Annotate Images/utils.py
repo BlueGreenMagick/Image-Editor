f02e69f52c9939da5a00b649b85c8bf2cbdfa3f0
BlueGreenMagick <bluegreenmagick@gmail.com>
2020-06-07 09:08:33 +0900

    change geom config should later try to hide _hidden

--- Annotate Images/utils.py
+++ Annotate Images/utils.py
@@ -8,14 +8,14 @@ def save_geom(widget, key):
     geom: QByteArray = widget.saveGeometry()
     geom_str = geom.toBase64().data().decode("ascii")
     config = addon.getConfig(__name__)
-    config["geom"][key] = geom_str
+    config["_hidden"]["geom"][key] = geom_str
     addon.writeConfig(__name__, config)
 
 
 def load_geom(key):
     config = addon.getConfig(__name__)
-    if key in config["geom"]:
-        geom = QByteArray.fromBase64(QByteArray.fromRawData(config["geom"][key].encode("ascii")))
+    if key in config["_hidden"]["geom"]:
+        geom = QByteArray.fromBase64(QByteArray.fromRawData(config["_hidden"]["geom"][key].encode("ascii")))
         return geom
     else:
         return None
