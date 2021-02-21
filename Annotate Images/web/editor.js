271a0f184d50fe9067fdccbcb5e2905e04ff5e6d
bluegreenmagick <bluegreenmagick@gmail.com>
2021-02-21 14:07:51 +0900

    use standardjs to format

--- Annotate Images/web/editor.js
+++ Annotate Images/web/editor.js
@@ -1,23 +1,20 @@
 (function () {
-    var targetEl = null;
-    window.addEventListener("contextmenu", function (e) {
-        targetEl = e.target;
-    })
-    window.addonAnno_getSrc = function(){
-        if(targetEl.tagName == "IMG"){
-            return targetEl.src
-        }else{
-            return null
-        }
+  let targetEl = null
+  window.addEventListener('contextmenu', function (e) {
+    targetEl = e.target
+  })
+  window.addonAnno_getSrc = function () {
+    if (targetEl.tagName === 'IMG') {
+      return targetEl.src
+    } else {
+      return null
     }
-    window.addonAnno_changeSrc = function(src){
-        var src = atob(src)
-        if(targetEl.tagName == "IMG"){
-            targetEl.src = src;
-        }else{
-            console.log("ERROR")
-        }
-        
+  }
+  window.addonAnno_changeSrc = function (src) {
+    if (targetEl.tagName === 'IMG') {
+      targetEl.src = window.atob(src)
+    } else {
+      console.log('ERROR')
     }
-
+  }
 })()
