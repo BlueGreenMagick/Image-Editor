7d74e9a30d5041be2ce7d5ee423171cb8187d170
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-05-10 12:03:04 +0900

    fix selected image finder in 2.1.50+

--- addon/web/editor.js
+++ addon/web/editor.js
@@ -1,15 +1,55 @@
 (function () {
   let targetEl = { tagName: "" }
+  let targetElOrd;
+  let targetField;
+  let verIsOver50 = false;
   const addonAnno = {}
 
-  addonAnno.addListener = function () {
-    const fields = document.getElementsByClassName('field')
-    for (let i = 0; i < fields.length; i++) {
-      let field = fields[i]
-      if (field.shadowRoot) {
-        field = field.shadowRoot
+  function getFromSvelteStore(store) {
+    let v;
+    const unsubscribe = store.subscribe((i) => { v = i });
+    unsubscribe();
+    return v
+  }
+
+  function currentField() {
+    if (typeof getCurrentField === 'function') {
+      const fieldEl = window.getCurrentField();
+      if (!fieldEl) return null;
+      return fieldEl.shadowRoot || fieldEl;
+    } else { // 2.1.50+
+      const noteEditor = window.require("anki/NoteEditor").instances[0];
+      const noteInput = getFromSvelteStore(noteEditor.focusedInput);
+      if (noteInput === null) return null;
+      return noteInput.element;
+    }
+  }
+
+  addonAnno.addListener = async function () {
+    let fields = [];
+    if (typeof getCurrentField === 'function') {
+      fieldEls = document.getElementsByClassName('field')
+      for (const el of fieldEls) {
+        fields.push(el.shadowRoot || el);
       }
+    } else { // 2.1.50+
+      await window.require("anki/ui").loaded;
+      const noteFields = window.require("anki/NoteEditor").instances[0].fields;
+      fields = await Promise.all(noteFields.map((field) => { return getFromSvelteStore(field.editingArea.editingInputs)[0].element }));
+      verIsOver50 = true;
+    }
+
+    for (const field of fields) {
       field.addEventListener('contextmenu', function (e) {
+        if (verIsOver50) {
+          targetField = field;
+          const images = field.getElementsByTagName("img");
+          for (let i = 0; i < images.length; i++) {
+            if (images[i] = e.target) {
+              targetElOrd = i;
+            }
+          }
+        }
         targetEl = e.target
       })
     }
@@ -20,9 +60,9 @@
   }
 
   const hasSingleImage = function () {
-    const field = window.getCurrentField()
+    const field = currentField();
     if (!field) { return false }
-    return field.shadowRoot.querySelectorAll('img').length === 1
+    return field.querySelectorAll('img').length === 1
   }
 
   addonAnno.imageIsSelected = function () {
@@ -38,10 +78,11 @@
   }
 
   addonAnno.changeSrc = function (src) {
-    if (targetEl.tagName === 'IMG') {
-      targetEl.src = window.atob(src)
+    if (verIsOver50) {
+      const target = targetField.getElementsByTagName("img")[targetElOrd];
+      target.src = atob(src);
     } else {
-      console.log('ERROR')
+      targetEl.src = atob(src)
     }
   }
 
